620c67218b118f7b545e35ab7a2a4a6729c2f584
giant-paw <giantpawbusiness@gmail.com>
2025-06-05 01:14:02 +0700

    membuat repository untuk profile admin dan menambahkan postWithToken

--- lib/service/service_http_client.dart
+++ lib/service/service_http_client.dart
@@ -48,5 +48,43 @@ class ServiceHttpClient {
     }
   }
 
-  // DELETE
+  Future<http.Response> postWihToken(
+    String endPoint,
+    Map<String, dynamic> body,
+  ) async {
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+
+      _logRequest("POST", url, body, response);
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  void _logRequest(
+    String method,
+    Uri url,
+    Map<String, dynamic>? body,
+    http.Response response,
+  ) {
+    print("==== HTTP REQUEST ====");
+    print("Method: $method");
+    print("URL: $url");
+    if (body != null) print("Body: ${jsonEncode(body)}");
+    print("Status Code: ${response.statusCode}");
+    print("Response Body: ${response.body}");
+    print("======================");
+  }
 }
