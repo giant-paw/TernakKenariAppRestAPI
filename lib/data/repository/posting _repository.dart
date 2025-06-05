d29a751743b27686f036c136e18bbc694513dc8f
giant-paw <giantpawbusiness@gmail.com>
2025-06-05 15:35:38 +0700

    menambah method getAllBurung

--- lib/data/repository/posting _repository.dart
+++ lib/data/repository/posting _repository.dart
@@ -5,6 +5,7 @@ import 'dart:developer';
 import 'package:dartz/dartz.dart';
 import 'package:materimei30/data/model/request/admin/posting_jual_request_model.dart';
 import 'package:materimei30/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:materimei30/data/model/response/get_all_burung_response_model.dart';
 import 'package:materimei30/service/service_http_client.dart';
 
 class PostingRepostory {
@@ -33,6 +34,24 @@ class PostingRepostory {
     } catch (e) {
       log("Error in Add burung : $e");
       return Left("An error occurred while post burung: $e");
+    }
+  }
+
+  Future<Either<String, GetAllBurungModel>> getAllBurung() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/burung-semua");
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final getAllBurung = GetAllBurungModel.fromMap(jsonResponse);
+
+        return Right(getAllBurung);
+      } else {
+        final jsonResponse = json.decode(response.body);
+
+        return Left(jsonResponse['message'] ?? "Get All burung failed");
+      }
+    } catch (e) {
+      return Left("An error occurred while getting all burung: $e");
     }
   }
 }
