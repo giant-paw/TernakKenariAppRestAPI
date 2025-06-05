12439d93ff6d6eae8a22975fdb7f5b6c9029cf7b
giant-paw <giantpawbusiness@gmail.com>
2025-06-05 14:58:51 +0700

    membuat anak repository dan method addAnak

---
+++ lib/data/repository/anak_repository.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:materimei30/data/model/request/admin/anak_request_model.dart';
+import 'package:materimei30/data/model/response/get_all_anak_response.dart';
+import 'package:materimei30/service/service_http_client.dart';
+
+class AnakRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  AnakRepository(this._serviceHttpClient);
+
+  Future<Either<String, GetAnakById>> addAnak(
+    AnakRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWihToken(
+        "admin/anak",
+        requestModel.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetAnakById.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+
+  
+}
