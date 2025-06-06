92e08b08268f50518aafcb39b588c67b00f3b409
giant-paw <giantpawbusiness@gmail.com>
2025-06-06 07:54:23 +0700

    membuat controller dan form di register screen

--- lib/presentation/auth/register_screen.dart
+++ lib/presentation/auth/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:materimei30/core/components/components.dart';
+import 'package:materimei30/core/components/spaces.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -8,8 +10,59 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+
+  late final TextEditingController namaController;
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  late final GlobalKey<FormState> _key;
+  bool isShowPassword = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SpaceHeight(100),
+                Text(
+                  'Daftar Akun Baru',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+
+                const SpaceHeight(30),
+                CustomTextField(
+                  controller: namaController, 
+                  label: 'Username', 
+                  validator: 'Username tidak boleh kosong',
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.person),
+                  ),
+                ),
+
+                const SpaceHeight(25),
+                CustomTextField(
+                  controller: emailController, 
+                  label: 'Email', 
+                  validator: 'Email tidak boleh kosong',
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.email), 
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
