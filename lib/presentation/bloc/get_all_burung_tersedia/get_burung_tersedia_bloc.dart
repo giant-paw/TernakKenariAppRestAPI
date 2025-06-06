29ff1aa19b8c4243bd85c38fc4629206c5d31b2b
giant-paw <giantpawbusiness@gmail.com>
2025-06-06 21:18:23 +0700

    bloc get burung tersedia

--- lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_bloc.dart
+++ lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_bloc.dart
@@ -1,14 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:materimei30/data/model/response/burung_semua_tersedia_model.dart';
-import 'package:meta/meta.dart';
+import 'package:materimei30/data/repository/get_all_burung_tersedia_repository.dart';
 
 part 'get_burung_tersedia_event.dart';
 part 'get_burung_tersedia_state.dart';
 
-class GetBurungTersediaBloc extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
-  GetBurungTersediaBloc() : super(GetBurungTersediaInitial()) {
-    on<GetBurungTersediaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class GetBurungTersediaBloc
+    extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
+  final GetAllBurungTersediaRepository getAllBurungTersediaRepository;
+
+  GetBurungTersediaBloc(this.getAllBurungTersediaRepository)
+    : super(GetBurungTersediaInitial()) {
+    on<GetAllBurungTersediaEvent>(_getAllBurungTersedia);
+  }
+
+  Future<void> _getAllBurungTersedia(
+    GetAllBurungTersediaEvent event,
+    Emitter<GetBurungTersediaState> emit,
+  ) async {
+    emit(GetBurungTersediaLoading());
+    final result = await getAllBurungTersediaRepository.getAllBurungTersedia();
+    result.fold(
+      (error) => emit(GetBurungTersediaError(message: error)),
+      (burungTersedia) =>
+          emit(GetBurungTersediaLoaded(burungTersedia: burungTersedia)),
+    );
   }
 }
